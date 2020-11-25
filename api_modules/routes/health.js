7b85162e013e4fb1838e91d3fe1f12fb3f3edc4b
Brickwall01 <20362490@students.latrobe.edu.au>
2020-11-25 15:20:13 +1100

    Version 1.0.0

--- api_modules/routes/health.js
+++ api_modules/routes/health.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const package = require.main.require('./package');
+//const package = require.main.require('./package');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
         Git_Hash: 'temp git hash',
-        App_Name: package.name,
-        Version: package.version
+        App_Name: process.env.npm_package_name,
+        Version: process.env.npm_package_version
         
     
     });
